6fbf889c963e25d8ad54fa1b88ee706435be30c4
Thiti <thiti.adev@gmail.com>
2021-03-13 16:14:50 +0700

    popularWords

--- __tests__/popularWords.test.ts
+++ __tests__/popularWords.test.ts
@@ -1,11 +1,12 @@
+import { popularWords } from './../src/popularWords';
 
-
-// describe('test fn backwardStringByWord', () => {
-//   test('check backwardStringByWord return', () => {
-//     expect(nonUniqueElements([1, 2, 3, 1, 3])).toEqual([1, 3, 1, 3]);
-//     expect(nonUniqueElements([1, 2, 3, 4, 5])).toEqual([]);
-//     expect(nonUniqueElements([5, 5, 5, 5, 5])).toEqual([5, 5, 5, 5, 5]);
-//     expect(nonUniqueElements([10, 9, 10, 10, 9, 8])).toEqual([10, 9, 10, 10, 9]);
-//     expect(nonUniqueElements([2])).toEqual([]);
-//   });
-// }) 
+describe('test fn popularWords', () => {
+  test('check popularWords return', () => {
+    expect(popularWords(`
+    When I was One
+    I had just begun  
+    When I was Two
+    I was nearly new`, ['i', 'was', 'three', 'near']))
+    .toEqual({ i: 4, was: 3, three: 0, near: 0 });
+  })
+});
--- src/popularWords.ts
+++ src/popularWords.ts
@@ -1,3 +1,9 @@
-export const popularWords = (text: string, words: string) => {
-
+export const popularWords = (text: string, words: string[]) => {
+    const str = text.toLowerCase().replace(/\n/g, ' ').split(' ');
+    const dic: any = {};
+    for( let i of words ){
+        dic[i] = str.filter(x => x == i).length;
+        console.log(`${i}: ${dic[i]}`)
+    }
+    return dic
 }
